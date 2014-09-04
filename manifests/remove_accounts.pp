2c65222077a4072e0614d707e599980e48c616c1
jason murray <jemurray@ubuntu1404.example.com>
2014-09-04 06:33:12 -0500

    Removed example accounts so accounts are not accidentally removed.

--- manifests/remove_accounts.pp
+++ manifests/remove_accounts.pp
@@ -3,17 +3,5 @@ class manageusers::remove_accounts() {
   ##
   ## Accounts that should be removed
   ##
-#   user { user3:
-#     ensure => "absent"
-#   }
-# 
-#   user { user4:
-#     ensure => "absent"
-#   }
-# 
-#   user { user7:
-#     ensure => "absent"
-#   }
-
 
 }
