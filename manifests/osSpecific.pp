0e99bb3d2822b9365dc50b61d207d1f30c6319db
Jason Murray <jemurray@shell.jasonmurray.org>
2015-10-31 03:42:54 +0000

    Added osSpecific name change

---
+++ manifests/osSpecific.pp
@@ -0,0 +1,22 @@
+# osSpecific 
+class manageusers::osSpecific () {
+
+  ##
+  ## Older versions of Ubuntu require libshadow to manage the passwords
+  ##
+  if $::operatingsystem =~ /Ubuntu|Debian/ and $::lsbdistrelease <= 12.04 {
+    package { 'libshadow-ruby1.8':
+      ensure => installed
+    }
+  }
+
+  ##
+  ## Redhat requires reby-shadow to manage passwords
+  ##
+  if $::operatingsystem =~ /RedHat|CentOS/  {
+    package { 'ruby-shadow':
+      ensure => installed
+    }
+  }
+
+}
